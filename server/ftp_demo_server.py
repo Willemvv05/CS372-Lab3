524f07b26fd0e376cbbb7e9be6979103ae9a6246
Willemvv05 <147097985+Willemvv05@users.noreply.github.com>
2024-06-04 13:35:32 -0700

    Update ftp_demo_server.py

--- server/ftp_demo_server.py
+++ server/ftp_demo_server.py
@@ -75,6 +75,100 @@ async def handle_client(reader, writer):
         writer.close()
         await writer.wait_closed()
         return
+    """
+    Part 2: FTP Protocol
+    """
+
+    # Enter command prompt loop
+    while (1):
+        # Get command from client
+        prompt = "Enter Command: "
+        await send_long_message(writer, prompt)
+        command = await receive_long_message(reader)
+        # Command Handlers
+        if command == "list":
+            # For list, get list and then send with ACK
+            fileList = os.listdir(path='.')
+            message = "ACK "
+            for i in fileList:
+                message += i + " "
+            await send_long_message(writer, message)
+        elif len(command) > 2 and command[:3] == "put":
+            # Get filename
+            filename = command[4:]
+            with open(filename, "x") as f:
+                # Get file contents and write them to new file
+                f.write(await receive_long_message(reader))
+            # Send Ack
+            await send_long_message(writer, "ACK")
+        elif len(command) > 2 and command[:3] == "get":
+            # Get filename and check if it exists
+            filename = command[4:]
+            if not os.path.isfile("./" + filename):
+                # If not, return NAK
+                await send_long_message(writer, "NAK Invalid Filename")
+                continue
+            # If file exists, send back ACK followed by contents
+            with open(filename, 'r') as f:
+                    await send_long_message(writer, ("ACK " + f.read()))
+        elif len(command) > 5 and command[:6] == "remove":
+            # Get filename and check if it exists
+            filename = command[7:]
+            if not os.path.isfile("./" + filename):
+                # If not, return NAK
+                await send_long_message(writer, "NAK Invalid Filename")
+                continue
+            # If file exists, send back ACK after deleting
+            os.remove("./" + filename)
+            await send_long_message(writer, "ACK")
+        elif len(command) > 4 and command[:5] == "close":
+            # Send ACK and end loop
+            await send_long_message(writer, "ACK")
+            break
+        else:
+            error = "NAK Invalid Command"
+            await send_long_message(writer, error)
+    return
+
+
+async def main():
+    server = await asyncio.start_server(
+            handle_client,
+            INTERFACE, SPORT
+    )
+
+    async with server:
+        await server.serve_forever()
+
+# Run the `main()` function
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    await writer.drain()
+
+
+# TODO: Implement me for Part 2!
+async def receive_long_message(reader: asyncio.StreamReader):
+    # First we receive the length of the message: this should be 8 total hexadecimal digits!
+    # Note: `socket.MSG_WAITALL` is just to make sure the data is received in this case.
+    data_length_hex = await reader.readexactly(8)
+
+    # Then we convert it from hex to integer format that we can work with
+    data_length = int(data_length_hex, 16)
+
+    full_data = await reader.readexactly(data_length)
+    return full_data.decode()
+
+
+async def handle_client(reader, writer):
+    """
+    Part 1: Introduction
+    """
+    # If password is invalid, end handle_client
+    if await send_intro_message(writer, reader, intro_message) == False:
+        writer.close()
+        await writer.wait_closed()
+        return
     return
     """
     Part 2: FTP Protocol
